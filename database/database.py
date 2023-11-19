9a2e283685290e88bca5733f0f8179cd7f942610
Lazaro Placeres <lrodriguezplaceres@gmail.com>
2023-11-18 23:05:14 -0500

    database, numero ilimitado de conexiones

--- database/database.py
+++ database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 # Create a sqlite engine instance
-engine = create_engine("sqlite:///punto.db")
+engine = create_engine("sqlite:///punto.db", max_overflow=-1)
 
 # Create a DeclarativeMeta instance
 Base = declarative_base()
